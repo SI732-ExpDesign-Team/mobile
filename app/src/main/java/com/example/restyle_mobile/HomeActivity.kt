154490e2337df693c72daf3fb5f83edc11910ccf
María Alejandra Díaz Villacrez <u202118315@upc.edu.pe>
2024-09-18 20:53:29 -0500

    us04(feature): added home screen buttons functionality

--- app/src/main/java/com/example/restyle_mobile/HomeActivity.kt
+++ app/src/main/java/com/example/restyle_mobile/HomeActivity.kt
@@ -1,22 +1,30 @@
 package com.example.restyle_mobile
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_home)
+
+        //Buttons
+        val signupButton: Button = findViewById(R.id.signup_button)
+        val loginButton: Button = findViewById(R.id.login_button)
+
+        signupButton.setOnClickListener { //Cambiar por SignupActivity
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        loginButton.setOnClickListener { //Cambiar por LoginActivity
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
         }
     }
-
 
 }
