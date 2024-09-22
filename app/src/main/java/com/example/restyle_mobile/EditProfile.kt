92a896fc65710052002d452ade91417cc53703e9
Beth Oneglio <106390542+bethnlg@users.noreply.github.com>
2024-09-22 16:57:38 -0500

    feat: add us03

---
+++ app/src/main/java/com/example/restyle_mobile/EditProfile.kt
@@ -0,0 +1,20 @@
+package com.example.restyle_mobile
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class EditProfile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_edit_profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.editprofile)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
