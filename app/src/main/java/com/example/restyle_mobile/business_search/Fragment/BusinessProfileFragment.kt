e1a86e522b6c19855f91450dc3761f62ec304ac9
María Alejandra Díaz Villacrez <u202118315@upc.edu.pe>
2024-10-18 19:58:07 -0500

    develop(fix): updated all retrofit paths

--- app/src/main/java/com/example/restyle_mobile/business_search/Fragment/BusinessProfileFragment.kt
+++ app/src/main/java/com/example/restyle_mobile/business_search/Fragment/BusinessProfileFragment.kt
@@ -73,7 +73,7 @@ class BusinessProfileFragment : Fragment() {
             .build()
 
         return Retrofit.Builder()
-            .baseUrl("https://restyle-backend.zeabur.app/")
+            .baseUrl("https://restyle-backend-v2.zeabur.app/")
             .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
